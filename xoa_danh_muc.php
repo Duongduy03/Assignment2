d9d4be99c6b84a7c26457269ef23e9d3af6e0b58
Duongduy03 <dvduy24@gmail.com>
2022-08-08 13:47:01 +0700

    Assignment2

--- xoa_danh_muc.php
+++ xoa_danh_muc.php
@@ -1,7 +1,7 @@
 <?php
     require_once('db.php');
     $category_id = $_GET['id'];
-    $sql = "DELETE FROM categories where id = $$category_id";
+    $sql = "DELETE FROM categories where id = $category_id";
     $statement = $connect -> prepare($sql);
     $statement->execute();
     header('location:quan_tri_danh_muc.php');
